0a1f2fac2862edac2f0aa6f904e5cb023c4a5d7a
William Munhoz <william_munhoz@outlook.com>
2025-08-25 21:53:19 -0300

    chore: inicializar projeto com dependências, estrutura de pastas e UserSchema

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import cors from "cors";
+
+config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+    console.error("❌ The MONGO_URI variable is not defined in the .env file");
+    process.exit(1);
+};
+
+app.get("/", (req, res) => res.send("🚀 Server initialized!"));
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        const server = app.listen(PORT, () => {
+            console.log(`🚀 Server is running on port: http://localhost:${PORT}`);
+        });
+
+        server.on("error", (error) => {
+            console.error("❌ Error on stating server", error.message);
+        });
+    })
+    .catch((error) => {
+        console.error("❌ Error connecting to database", error);
+    })
